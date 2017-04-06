47c1861e14db8bf62cbb21da7be17b65bf69d435
CALDWELL <caldnath492@edu.gedsb.local>
2017-04-06 12:47:33 -0400

    first commit

---
+++ Word Tally/Word Tally/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Word_Tally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("passage.txt");
+            data = data.ToLower();
+
+            for (int n = 0; n >= data.Length; n++) {
+                if (Convert.ToInt32(data[n]) < Convert.ToInt32("a") || Convert.ToInt32(data[n]) > Convert.ToInt32("z") || Convert.ToInt32(data[n]) != Convert.ToInt32(" ")) {
+                    data.Remove(data[n], 1);
+                }
+                string[] List = data.Split(' ');
+                Dictionary<string, int> dit = new Dictionary<string, int>();
+                for (int i = 0; i <= List.Length; i++) {
+                    if (dit.ContainsKey(List[i]) == false)
+                    {
+                        dit.Add(List[i], 1);
+                    }
+                    else if (dit.ContainsKey(List[i]) == true) {
+                        int count;
+                        dit.TryGetValue(List[i], out count);
+                        count++;
+                        dit.Remove(List[i]);
+                        dit.Add(List[i], count);
+                    }                    
+                }
+            }
+            StreamWriter sw = new StreamWriter("tallies.txt");
+            sw.WriteLine("WORD---------------------COUNT");
+            sw.WriteLine("------------------------------");
+        }
+    }
+}
---
+++ Word Tally/Word Tally/README.txt
@@ -0,0 +1,23 @@
+﻿ __      __                .___ ___________      .__  .__         
+/  \    /  \___________  __| _/ \__    ___/____  |  | |  | ___.__.
+\   \/\/   /  _ \_  __ \/ __ |    |    |  \__  \ |  | |  |<   |  |
+ \        (  <_> )  | \/ /_/ |    |    |   / __ \|  |_|  |_\___  |
+  \__/\  / \____/|__|  \____ |    |____|  (____  /____/____/ ____|
+       \/                   \/                 \/          \/     
+
+PROBLEM DESCRIPTION
+---------------------------------------------------------------------------------------------------
+Write a program to tally the instances of words in a text passage.
+
+INPUT SPECIFICATION
+---------------------------------------------------------------------------------------------------
+The input file "passage.txt" contains the text passage.
+
+OUTPUT SPECIFICATION
+---------------------------------------------------------------------------------------------------
+The output file "tallies.txt" contains the count for each word in the passage.  Be sure to match
+the sample output exactly.
+
+RESTRICTIONS
+---------------------------------------------------------------------------------------------------
+There are no restrictions for this challenge.  You can use any language features.
